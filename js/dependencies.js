5b560f5ecc7722f3906ad186b9c5ee990ac12531
Dana Scheider <dana.scheider@gmail.com>
2015-03-09 12:42:21 -0700

    Require jQuery cookie and API module

--- js/dependencies.js
+++ js/dependencies.js
@@ -2,7 +2,8 @@ var Canto = {
   $          : require('jquery'),
   _          : require('underscore'),
   Backbone   : require('backbone'),
-  cookie     : require('jquery.cookie')
+  cookie     : require('jquery.cookie'),
+  API        : require(process.cwd() + '/js/api.js')
 };
 
 module.exports = Canto;
