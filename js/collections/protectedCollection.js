010330730653dca63b76b26047aff1bd2b5730fb
Dana Scheider <dana.scheider@gmail.com>
2015-03-10 14:20:49 -0700

    First spec for ProtectedCollection fetch method

--- js/collections/protectedCollection.js
+++ js/collections/protectedCollection.js
@@ -1,10 +1,27 @@
 var App = require(process.cwd() + '/js/dependencies.js');
+var Backbone       = App.Backbone;
 var $              = App.$;
 var context        = describe; // RSpecify
 
-var ProtectedCollection = App.Backbone.Collection.extend({
+var ProtectedCollection = Backbone.Collection.extend({
   token : function() {
     return 'Basic ' + $.cookie('auth');
+  },
+
+  // ------------------------- //
+  // Core Collection Functions //
+  // ------------------------- //
+
+  fetch : function(opts) {
+    opts = opts || {};
+
+    var that = this;
+
+    opts.beforeSend = (opts.beforeSend) || function(xhr) {
+      xhr.setRequestHeader('Authorization', that.token());
+    };
+
+    return Backbone.Collection.prototype.fetch.call(this, opts);
   }
 });
 
--- spec/collections/protectedCollectionSpec.js
+++ spec/collections/protectedCollectionSpec.js
@@ -8,19 +8,35 @@ var SUT = require(process.cwd() + '/js/collections/protectedCollection.js');
 var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 var Backbone       = App.Backbone;
 var $              = Backbone.$ = App.$;
+var Model          = Backbone.Model.extend({});
 var context        = describe; // RSpecify
 
 describe('Protected Collection', function() {
-  var collection, model1, model2, model3;
+  var collection, model1, model2, model3, xhr;
 
   beforeEach(function() {
-    collection = new SUT();
+    collection = new SUT({model: Model});
+    collection.url = App.API.base + '/models';
+    xhr = new XMLHttpRequest();
     spyOn($, 'cookie').and.returnValue(Env.btoa('testuser:testuser'));
   });
 
   describe('token', function() {
     it('returns the value of the auth header for the logged-in user', function() {
       expect(collection.token()).toEqual('Basic ' + Env.btoa('testuser:testuser'));
+    });
+  });
+
+  describe('core functions', function() {
+    describe('fetch', function() {
+      beforeEach(function() { spyOn($, 'ajax'); });
+
+      it('attaches the auth header', function() {
+        xhr.open('GET', collection.url);
+        collection.fetch();
+        $.ajax.calls.argsFor(0)[0].beforeSend(xhr);
+        expect(xhr.getRequestHeader('Authorization')).toEqual(collection.token());
+      });
     });
   });
 });
