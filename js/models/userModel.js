7010cc23f31d10bab40cc27e90de81b5c2871b59
Dana Scheider <dana.scheider@gmail.com>
2015-03-10 12:44:04 -0700

    First test for user constructor

--- js/models/userModel.js
+++ js/models/userModel.js
@@ -3,7 +3,25 @@ var App = require(process.cwd() + '/js/dependencies.js');
 var Backbone = App.Backbone, _ = App._, $ = App.$;
 
 var UserModel = Backbone.Model.extend({
-  urlRoot: App.API.users.collection
+  urlRoot: App.API.users.collection,
+
+  // ----------------- //
+  // Special Functions //
+  // ----------------- //
+
+  protectedFetch : function() {
+    //
+  },
+
+  // -------------------- //
+  // Core Model Functions //
+  // -------------------- //
+
+  initialize     : function(attrs, opts) {
+    opts = opts || {};
+    
+    if(this.get('id')) { this.protectedFetch(); }
+  }
 });
 
 module.exports = UserModel;
--- spec/userModelSpec.js
+++ spec/userModelSpec.js
@@ -6,6 +6,7 @@ var SUT = require(process.cwd() + '/js/models/userModel.js');
 
 var Backbone          = App.Backbone;
 var $                 = Backbone.$ = App.$;
+var context           = describe // RSpecify
 
 describe('User Model', function() {
   var user;
@@ -21,6 +22,16 @@ describe('User Model', function() {
   describe('properties', function() {
     it('has `urlRoot` /users', function() {
       expect(user.urlRoot).toEqual(App.API.base + '/users');
+    });
+  });
+
+  describe('constructor', function() {
+    context('when instantiated with an ID', function() {
+      it('calls protectedFetch', function() {
+        spyOn(SUT.prototype, 'protectedFetch');
+        var newUser = new SUT({id: 14});
+        expect(SUT.prototype.protectedFetch).toHaveBeenCalled();
+      });
     });
   });
 });
