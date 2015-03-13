9998bcd78fe74c5d10db4e56231116e16dd4267a
Dana Scheider <dana.scheider@gmail.com>
2015-03-13 12:16:29 -0700

    Add main.js file for browserify

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Require main dependencies
+require('./dependencies.js');
+
+// Require models
+require('./models/protectedResourceModel.js');
+require('./models/taskModel.js');
+require('./models/userModel.js');
