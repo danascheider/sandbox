055b29650d8878d5116dfe43f9583633e7c4b068
Dana Scheider <dana.scheider@gmail.com>
2015-03-13 12:23:19 -0700

    Finish requiring existing app elements

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@ require('./dependencies.js');
 require('./models/protectedResourceModel.js');
 require('./models/taskModel.js');
 require('./models/userModel.js');
+
+// Require collections
+require('./collections/protectedCollection.js');
+require('./collections/taskCollection.js');
+
+// Require model views
+require('./views/modelViews/taskViews/taskModelView.js');
+require('./views/modelViews/taskViews/taskListItemView.js');
+
+// Require API and API options
+require('./apiOptions.js');
+require('./api.js');
