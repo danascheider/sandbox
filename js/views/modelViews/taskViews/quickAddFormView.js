01e77535c893828ffa93a27ded9c2e5feda2ad9f
Dana Scheider <dana.scheider@gmail.com>
2015-03-21 14:18:36 -0700

    Assign grouping to quick-add form in constructor

--- js/views/modelViews/taskViews/quickAddFormView.js
+++ js/views/modelViews/taskViews/quickAddFormView.js
@@ -1,7 +1,10 @@
 require('../../../dependencies.js');
 
 var QuickAddFormView = Backbone.View.extend({
-  //
+  initialize: function(opts) {
+    opts = opts || {};
+    this.grouping = opts.grouping;
+  }
 });
 
 module.exports = QuickAddFormView;
--- spec/views/modelViews/quickAddFormViewSpec.js
+++ spec/views/modelViews/quickAddFormViewSpec.js
@@ -10,18 +10,18 @@ var TaskCollection = require(process.cwd() + '/js/collections/taskCollection.js'
 Backbone.$         = $;
 var context        = describe; // RSpecify
 
-var task1 = new TaskModel({id: 1, title: 'Test Task 1'}),
-    task2 = new TaskModel({id: 2, title: 'Test Task 2'}),
-    task3 = new TaskModel({id: 3, title: 'Test Task 3'});
+var task1 = new TaskModel({id: 1, title: 'Test Task 1', status: 'Blocking'}),
+    task2 = new TaskModel({id: 2, title: 'Test Task 2', status: 'Blocking'}),
+    task3 = new TaskModel({id: 3, title: 'Test Task 3', status: 'Blocking'});
 
 var collection = new TaskCollection([task1, task2, task3]);
 
-describe('#travis Quick-Add Task Form', function() {
+describe('Quick-Add Task Form', function() {
   var view;
 
   beforeEach(function() {
     jasmine.addMatchers(matchers);
-    view = new SUT({collection: collection});
+    view = new SUT({collection: collection, grouping: {status: 'Blocking'}});
   });
 
   afterAll(function() {
@@ -30,14 +30,18 @@ describe('#travis Quick-Add Task Form', function() {
   });
 
   describe('constructor', function() {
-    it('assigns the collection', function() {
+    it('#travis assigns the collection', function() {
       expect(view.collection).toBe(collection);
     });
 
-    it('doesn\'t call render', function() {
+    it('#travis doesn\'t call render', function() {
       spyOn(SUT.prototype, 'render');
       var newView = new SUT({collection: collection});
       expect(SUT.prototype.render).not.toHaveBeenCalled();
+    });
+
+    it('#travis sets the `grouping` property', function() {
+      expect(view.grouping).toEqual({status: 'Blocking'});
     });
   });
 });
