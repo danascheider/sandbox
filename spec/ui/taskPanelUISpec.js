3b6ea070ae309854d6064ae0a15ba42bb9acf0e5
Dana Scheider <dana.scheider@gmail.com>
2015-03-28 21:34:25 -0700

    Passing tests for hideToggleWidgetIcon, showWidget, and hideWidget

--- spec/ui/taskPanelUISpec.js
+++ spec/ui/taskPanelUISpec.js
@@ -1,7 +1,5 @@
 require(process.cwd() + '/js/dependencies.js');
-
 require(process.cwd() + '/spec/support/webdriver.js');
-require(process.cwd() + '/js/dependencies.js');
 require(process.cwd() + '/spec/support/env.js');
 
 var matchers = require('jasmine-jquery-matchers'),
@@ -9,20 +7,72 @@ var matchers = require('jasmine-jquery-matchers'),
     fcontext = fdescribe;
 
 describe('Task Panel View Elements', function() {
+  beforeAll(function(done) {
+    jasmine.addMatchers(matchers);
+    client.init().url('http://localhost/#taskPanelViewSpec', done);
+  });
+
+  beforeEach(function(done) {
+    client.refresh();
+    done();
+  });
+
+  afterAll(function(done) {
+    client.end();
+    done();
+  });
+
   describe('hideToggleWidgetIcon', function() {
-    it('hides the toggle-widget icon');
+    beforeEach(function(done) {
+      client.waitForVisible('a[data-method=displayIcon]')
+            .click('a[data-method=displayIcon]', done);
+    });
+
+    it('hides the toggle-widget icon', function(done) {
+      client.waitForVisible('a[data-method=hideToggleWidgetIcon]')
+            .click('a[data-method=hideToggleWidgetIcon]')
+            .waitForVisible('#task-panel i.toggle-widget', function(err, isVisible) {
+        
+        expect(isVisible).toBe(false);
+        done();
+      });
+    });
   });
 
   describe('hideWidget', function() {
-    it('hides the panel body');
+    beforeEach(function(done) {
+      client.waitForVisible('#task-panel .panel-body', done)
+    });
+
+    it('hides the panel body', function(done) {
+      client.click('#triggers a[data-method=hideWidget]')
+            .waitForVisible('#task-panel .panel-body', 1500, true) 
+            .selectorExecute('#task-panel .panel-body', function(panelBody) {
+          
+        expect(panelBody).not.toBeVisible();
+      }, done);
+    });
   });
 
   describe('showToggleWidgetIcon', function() {
     it('shows the toggle-widget icon');
   });
 
-  describe('showWidget', function() {
-    it('shows the panel body');
+  fdescribe('showWidget', function() {
+    beforeEach(function(done) {
+      client.waitForVisible('a[data-method=hidePanelBody]')
+            .click('a[data-method=hidePanelBody]')
+            .waitForVisible('#task-panel .panel-body', 1500, true, done)
+    });
+
+    it('shows the panel body', function(done) {
+      client.click('a[data-method=showWidget]')
+            .waitForVisible('#task-panel .panel-body', 1500)
+            .selectorExecute('#task-panel .panel-body', function(panelBody) {
+
+        expect(panelBody).toBeVisible();
+      }, done);
+    });
   });
 
   describe('remove', function() {
