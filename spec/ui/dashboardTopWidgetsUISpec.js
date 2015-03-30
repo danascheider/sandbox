820d9a82440c5cee7ab5d617c314742be8d70532
Dana Scheider <dana.scheider@gmail.com>
2015-03-29 18:34:09 -0700

    First spec for dashboard top widgets & clean up tags on sidebar specs

--- spec/ui/dashboardTopWidgetsUISpec.js
+++ spec/ui/dashboardTopWidgetsUISpec.js
@@ -1,3 +1,51 @@
+
+/* Core Requires
+/*************************************************************************/
+
+require(process.cwd() + '/spec/support/webdriver.js');
+require(process.cwd() + '/js/dependencies.js');
+require(process.cwd() + '/spec/support/env.js');
+
+var matchers = require('jasmine-jquery-matchers'),
+    context  = describe,
+    fcontext = fdescribe;
+
+/****************************************************************************
+ *                                                                          *
+ * BEGIN SUITE                                                              *
+ *                                                                          *  
+/****************************************************************************/
+
+describe('Dashboard Top Widgets View #ui', function() {
+
+  /* Filters
+  /***********************************************************************/
+
+  beforeAll(function(done) {
+    client.init().url('http://localhost/#dashboardTopWidgetViewSpec', done);
+  });
+
+  beforeEach(function(done) {
+    client.refresh();
+    done();
+  });
+
+  afterAll(function() {
+    client.end();
+  });
+
+  describe('view elements', function() {
+    describe('task widget', function() {
+      it('is visible by default', function(done) {
+        client.waitForVisible('#view div[data-name=tasks]', function(err, isVisible) {
+          expect(isVisible).toBe(true);
+          done();
+        });
+      });
+    });
+  });
+});
+
 // Items to be tested:
 //   - task widget is visible by default
 //   - deadline widget is visible by default
