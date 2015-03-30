cc8ec66ee2f1d5c0390732bff1cb164b44ef0031
Dana Scheider <dana.scheider@gmail.com>
2015-03-30 13:06:54 -0700

    Make stock tests pass for the dashboardHomeView

---
+++ js/views/partialViews/dashboardHomeView.js
@@ -0,0 +1,73 @@
+/***************************************************************************
+ *                                                                         *
+ * DASHBOARD HOME VIEW                                                     *
+ *                                                                         *
+ * The DashboardHomeView is the view the user sees when they first log     *
+ * into their dashboard. It contains summary information about all their   *  
+ * activities and obligations.                                             *
+ *                                                                         *
+ * CONTENTS                                                          LINE  *
+ * Core Requires .................................................... 30   *
+ * Module-Specific Requires ......................................... 36   *
+ * Module ........................................................... 44   *
+ *   Backbone View Properties ....................................... 50   *
+ *   Canto View Properties .......... ............................... 60   *
+ *     klass                                                               *
+ *     family                                                              *
+ *     superFamily                                                         *
+ *     types                                                               *
+ *   Core Functions ................................................. 69   *
+ *     fetch()                                                             *
+ *   Special Functions .............................................. 91   *
+ *     updateAll() .................................................. 92   *
+ *     isA() ....................................................... 140   *
+ *                                                                         *
+/****************************************************************************/
+
+/* Core Requires
+/****************************************************************************/
+
+Canto      = Canto || require('../../dependencies.js');
+Canto.View = Canto.View || require('../appViews/cantoView.js');
+
+/* Module-Specific Requires
+/****************************************************************************/
+
+var User          = require('../../models/userModel.js'),
+    TopWidgetView = require('./dashboardTopWidgetView.js'),
+    TaskPanelView = require('./taskPanelView.js');
+
+/****************************************************************************
+ * BEGIN MODULE                                                             *
+/****************************************************************************/
+
+var DashboardHomeView = Canto.View.extend({
+
+  /* Backbone View Properties
+  /**************************************************************************/
+
+  /* Canto View Properties
+  /**************************************************************************/
+
+  klass       : 'DashboardHomeView',
+  family      : 'Canto.View',
+  superFamily : 'Backbone.View',
+  types       : function() {
+    return Canto.View.prototype.types().concat(['DashboardHomeView', 'PartialView']);
+  },
+
+  /* View Events
+  /**************************************************************************/
+
+  /* Event Callbacks
+  /**************************************************************************/
+
+  /* Special Functions
+  /**************************************************************************/
+
+  /* Core View Functions
+  /**************************************************************************/
+
+});
+
+module.exports = DashboardHomeView;
---
+++ spec/views/partialViews/dashboardHomeViewSpec.js
@@ -0,0 +1,120 @@
+/****************************************************************************
+ *                                                                         *
+ * DASHBOARD HOME VIEW                                                     *
+ *                                                                         *
+ * The DashboardHomeView is the view the user sees when they first log     *
+ * into their dashboard. It contains summary information about all their   *
+ * activities and obligations.                                             *
+ *                                                                         *
+ * CONTENTS                                                          LINE  *
+ * Requires ......................................................... 26   *
+ * Suite ............................................................ 44   *
+ *   Filters ........................................................ 50   *
+ *   Authorization and Authentication ............................... 60   *
+ *     token()                                                             *
+ *   Core Functions ................................................. 69   *
+ *     fetch()                                                             *
+ *   Special Functions .............................................. 91   *
+ *     updateAll() .................................................. 92   *
+ *     isA() ....................................................... 140   *
+ *                                                                         *
+/****************************************************************************/
+
+/* Core Requires
+/****************************************************************************/
+
+require(process.cwd() + '/spec/support/jsdom.js');
+require(process.cwd() + '/js/dependencies.js');
+require(process.cwd() + '/spec/support/env.js');
+
+var fixtures       = require(process.cwd() + '/spec/support/fixtures/fixtures.js'),
+    matchers       = _.extend(require(process.cwd() + '/spec/support/matchers/toBeA.js'), require('jasmine-jquery-matchers')),
+    context        = describe,
+    fcontext       = fdescribe;
+
+var SUT = require(process.cwd() + '/js/views/partialViews/dashboardHomeView.js');
+
+/******************************************************************************
+ *                                                                            *
+ * BEGIN SUITE                                                                *
+ *                                                                            *  
+/******************************************************************************/
+
+fdescribe('Dashboard Home View #travis', function() {
+  var view;
+
+  /* Filters
+  /****************************************************************************/
+
+  beforeAll(function() {
+    _.extend(global, fixtures);
+  });
+
+  beforeEach(function() {
+    view = new SUT({user: user});
+  });
+
+  afterEach(function() {
+    restoreFixtures();
+  });
+
+  afterAll(function() {
+    view.remove();
+    view = null;
+    global = _.omit(global, fixtures);
+  });
+
+  /* Static Properties
+  /****************************************************************************/
+
+  describe('properties', function() {
+    it('has klass DashboardHomeView', function() {
+      expect(view.klass).toEqual('DashboardHomeView');
+    });
+
+    it('has family Canto.View', function() {
+      expect(view.family).toEqual('Canto.View');
+    });
+
+    it('has superFamily Backbone.View', function() {
+      expect(view.superFamily).toEqual('Backbone.View');
+    });
+  });
+
+  /* View Constructor
+  /**************************************************************************/
+
+  describe('constructor', function() {
+    it('does not call render', function() {
+      spyOn(SUT.prototype, 'render');
+      var newView = new SUT();
+      expect(SUT.prototype.render).not.toHaveBeenCalled();
+    });
+  });
+
+  /* Event Wiring
+  /**************************************************************************/
+
+  /* Event Callbacks
+  /**************************************************************************/
+
+
+  /* Special Functions
+  /**************************************************************************/
+
+  describe('special functions', function() {
+    describe('isA', function() {
+      it('returns true with argument DashboardHomeView', function() {
+        expect(view.isA('DashboardHomeView')).toBe(true);
+      });
+
+      it('returns true with argument PartialView', function() {
+        expect(view.isA('PartialView')).toBe(true);
+      });
+
+      it('returns false with another argument', function() {
+        expect(view.isA('Corvette')).toBe(false);
+      });
+    });
+  });
+});
