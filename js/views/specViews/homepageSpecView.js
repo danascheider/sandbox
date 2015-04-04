25b24f6d31ab4d6d50dbc03582ed03c2d80e29ca
Dana Scheider <dana.scheider@gmail.com>
2015-04-04 10:58:17 -0700

    Put triggers at the top instead of the bottom for the homepage view since it's long

--- js/views/specViews/homepageSpecView.js
+++ js/views/specViews/homepageSpecView.js
@@ -7,14 +7,34 @@ var SpecWrapper = Canto.View.extend({
   el         : 'body',
   template   : JST['spec/homepage'],
 
-  events     : {
-    'click a[data-method=showLoginForm]' : 'callShowLoginForm'
+  events  : {
+    'click a[data-method=showLoginForm]'   : 'showLoginForm',
+    'click a[data-method=hideLoginForm]'   : 'callHideLoginForm',
+    'click a[data-method=toggleLoginForm]' : 'callToggleLoginForm'
   },
 
-  callShowLoginForm: function(e) {
+  /* Event Callbacks
+  /***************************************************************************/
+
+  callHideLoginForm   : function(e) {
     e.preventDefault();
-    this.view.showLoginForm();
+    this.view.hideLoginForm($.Event('dblclick', {target: view.$('#shade')}));
   },
+
+  callToggleLoginForm : function(e) {
+    e.preventDefault();
+    this.view.toggleLoginForm($.Event('click', {target: view.$('.login-link')}));
+  },
+
+  showLoginForm       : function(e) {
+    e.preventDefault();
+    if(!this.view.$('#login-form').is(':visible')) {
+      this.view.toggleLoginForm($.Event('click', {target: view.$('.login-link')}));
+    }
+  },
+
+  /* Core View Functions
+  /***************************************************************************/
 
   initialize : function() {
     this.view = new HomepageView();
